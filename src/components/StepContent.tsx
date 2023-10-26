7a4d012c28152c143ac725b65f1124a5f4547014
Dominik Rubröder <dominik.rubroeder@mediawave.de>
2023-10-26 14:37:56 +0200

    Animate in new StepContent

--- src/components/StepContent.tsx
+++ src/components/StepContent.tsx
@@ -1,9 +1,23 @@
-import {ReactNode} from "react";
+"use client";
+import { ReactNode, useEffect } from "react";
+import { useAnimate } from "framer-motion";
 
 interface StepContentProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-export default function StepContent({children}: StepContentProps) {
-    return <div className="animate-fadeUp">{children}</div>
+export default function StepContent({ children }: StepContentProps) {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    if (children) {
+      animate(
+        scope.current,
+        { opacity: [0, 1], y: [16, 0] },
+        { ease: "easeOut", duration: 0.3 },
+      );
+    }
+  }, [children, animate, scope]);
+
+  return <div ref={scope}>{children}</div>;
 }
